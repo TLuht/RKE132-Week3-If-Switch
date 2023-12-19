160b03249f32480acca32920bef887946ccb45bb
TLuht <thomas.luht@tktk.ee>
2023-12-19 23:28:52 +0200

    if and switch

---
+++ Grade/Program.cs
@@ -0,0 +1,18 @@
+﻿char grade;
+Console.WriteLine("What grade did you receive?");
+grade = Convert.ToChar(Console.ReadLine().ToUpper());
+
+if (grade == 'A')
+    Console.WriteLine("Suurepärane!");
+else if (grade == 'B')
+    Console.WriteLine("Väga hea!");
+else if (grade == 'C')
+    Console.WriteLine("Hea!");
+else if (grade == 'D')
+    Console.WriteLine("Rahuldav");
+else if (grade == 'E')
+    Console.WriteLine("Kasin");
+else if (grade == 'F')
+    Console.WriteLine("Puudulik");
+else
+    Console.WriteLine("Vale väärtus!");
---
+++ SwitchDayOfTheWeek/Program.cs
@@ -0,0 +1,31 @@
+﻿//võtab kuupäeva väärtuse
+int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+//võrdleb saadud väärtust, et saada kuupäev
+switch (weekDay)
+{
+    case 0:
+        Console.WriteLine("It's Sunday!");
+        break;
+    case 1:
+        Console.WriteLine("It's Monday!");
+        break;
+    case 2:
+        Console.WriteLine("Its's Tuesday!");
+        break;
+    case 3:
+        Console.WriteLine("It's Wednesday!");
+        break;
+    case 4:
+        Console.WriteLine("It's Thursday!");
+        break;
+    case 5:
+        Console.WriteLine("It's Friday!");
+        break;
+    case 6:
+        Console.WriteLine("It's Saturday");
+        break;
+    default:
+        Console.WriteLine("Invalid date");
+        break;
+}
